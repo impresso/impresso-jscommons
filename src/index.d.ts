b5b0a372e41c27e7d2ee34faa699175d869fd9bd
Roman Kalyakin <roman@kalyakin.com>
2020-04-13 10:04:49 +0200

    more types

--- src/index.d.ts
+++ src/index.d.ts
@@ -47,9 +47,29 @@ export interface SearchQuery {
   filters: Filter[]
 }
 
+export declare namespace protobuf {
+  export namespace filter {
+    function serialize(filter: Filter): string
+    function deserialize(serializedFilter: string): Filter
+  }
+  export namespace searchQuery {
+    function serialize(searchQuery: SearchQuery): string
+    function deserialize(serializedSearchQuery: string): SearchQuery
+  }
+}
+
 export declare namespace logic {
   export namespace filter {
     function optimizeFilters(filters: Filter[]): Filter[]
     function mergeFilters(filtersSets: Filter[]): Filter[]
+  }
+}
+
+export declare namespace constants {
+  export namespace filter {
+    const Types: string[]
+    const Operators: string[]
+    const Contexts: string[]
+    const Precision: string[]
   }
 }
